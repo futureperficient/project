46fb4eec67de71df069110a21cdf390ef48942da
yeswanth/project <59158544+futureperficient@users.noreply.github.com>
2020-02-05 15:08:33 +0530

    Add files via upload

---
+++ app/models/SocialUser.js
@@ -0,0 +1,15 @@
+
+const mongoose = require('mongoose');
+//creating object 
+const Schema = mongoose.Schema;
+
+//Schema for user
+const socialUserSchema = new Schema({
+
+	id               : {type: String },
+    name             : {type: String },
+    email            : {type: String}
+});
+
+//model for userschema
+module.exports = mongoose.model('SocialUser' , socialUserSchema);
